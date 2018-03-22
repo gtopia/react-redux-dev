bc0059f637512c2d8d56a45a1ee98009b8b2c9f1
gtopia <gtopia@163.com>
2018-03-23 01:00:23 +0800

    Clean code.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,52 +1,14 @@
 import './index.scss';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
-import * as AppActions from '../../actions/app';
-import Browser from '../../static/util/browser';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            'location': {}
-        };
-    }
-
-    componentWillMount() {
-        this.setState({
-            'location': Object.assign({}, this.props.history.location)
-        });
-    }
-
-    shouldComponentUpdate(nextProps) {
-        if (JSON.stringify(nextProps.appState) === JSON.stringify(this.props.appState) &&
-            JSON.stringify(nextProps.history.location) === JSON.stringify(this.state.location)) {
-            return false;
-        }
-        else {
-            return true;
-        }
     }
 
     render() {
-        const { 
-            hasMoreTopic, 
-            userInfo, 
-            isWant2Logout, 
-            isShowMe
-        } = this.props.appState;
-        const { 
-            handleLogin,
-            want2Logout, 
-            toggleMe, 
-            cancelLogout, 
-            handleLogout, 
-            checkLoginStatus
-        } = this.props.appActions;
-
         return (
             <main className="layout__app">
                 { this.props.children }
@@ -56,24 +18,7 @@ class App extends Component {
 }
 
 App.propTypes = {
-    history: PropTypes.object,
-    appState: PropTypes.object.isRequired,
-    appActions: PropTypes.object.isRequired
+    history: PropTypes.object
 };
 
-function mapStateToProps(state) {
-    return {
-        appState: state.app.toJS()
-    };
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        appActions: bindActionCreators(AppActions, dispatch)
-    };
-}
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App));
+export default withRouter(App);
