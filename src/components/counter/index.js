8cbdb1241090068a18759493b135c235db4b9431
gtopia <gtopia@163.com>
2018-03-23 00:37:18 +0800

    Add counter component.

---
+++ src/components/counter/index.js
@@ -0,0 +1,38 @@
+import './index.scss';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return JSON.stringify(nextState) !== JSON.stringify(this.state);
+    }
+
+    render() {
+        let loadingClass = classNames({
+            'counter__loading': true,
+            'hide': this.state.isGetAll
+        });
+
+        return (
+            <div className="layout__counter">
+                <button>-</button>
+                <span>{0}</span>
+                <button>+</button>
+                <div className={loadingClass}></div>
+            </div>
+        );
+    }
+}
+
+Counter.propTypes = {
+    history: PropTypes.object,
+
+};
+
+export default withRouter(Counter);
